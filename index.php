aab1eaf1c954614c9404dbb05eb3fe3124e8d25c
Lior Bas <look551@gmail.com>
2024-11-14 20:44:47 +0200

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// This is a single line comment
+/*
+Line1
+line2
+line3
+*/
+echo 'Hello From PHP';
+echo '<br />';
+print 'Hello from print';
+echo '<br />';
+echo 'Value One', 'Value Tow', 'Value Three';
