1aa6ba2f69e441fcdc46fb281065004f474b981b
Lior Bas <look551@gmail.com>
2024-11-19 11:20:37 +0200

    const

---
+++ functions/constants.php
@@ -0,0 +1,20 @@
+<?php
+
+define('APP_NAME', 'My App');
+define('APP_VERSION', '1.0.0');
+
+
+const DB_NAME = "mydb";
+const DB_HOST = 'localHost';
+
+
+function run()
+{
+    echo APP_NAME;
+    echo DB_NAME, DB_HOST;
+}
+
+run();
+
+// define('APP_NAME', 'My App 2');
+// const DB_NAME = "test";
